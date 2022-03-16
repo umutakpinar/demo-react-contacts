000503bc8a06bfcc167d7b524ca5bc11dac82cd4
Umut Akpinar <umtakpnr5@gmail.com>
2022-03-16 20:56:34 +0300

    form UI updated

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -1,13 +1,51 @@
 //Form index.js
 
-import React from 'react'
+import React from "react";
 
 function index() {
-    return (
-        <>
-            <h3 className=''>Form Section</h3>
-        </>
-    )
+  return (
+    <>
+      <div className="container">
+        <form>
+          <div className="input-group  mb-3">
+            <label
+              htmlFor="fullName"
+              className="input-group-prepend input-group-text"
+            >
+              Person :
+            </label>
+            <input
+              id="fullName"
+              type="text"
+              placeholder="Full Name"
+              className=" form-control input-group-append"
+            />
+          </div>
+
+          <div className="input-group  mb-3">
+            <label
+              htmlFor="number"
+              className="input-group-prepend input-group-text"
+            >
+              Number :
+            </label>
+            <input
+              id="number"
+              type="text"
+              placeholder="0555 555 55 55"
+              className="form-control input-group-append"
+            />
+          </div>
+
+          <div className="btn-group-sm float-right">
+            <button className="btn-lg btn-primary mr-1">Add</button>
+            <button className="btn-lg btn-danger">Remove</button>
+          </div>
+
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default index
+export default index;
